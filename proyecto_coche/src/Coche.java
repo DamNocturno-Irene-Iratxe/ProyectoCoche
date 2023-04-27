ee0b69bec812b9c597fd8269ea71f75fc649bd5c
IreneSarasua <129441815+IreneSarasua@users.noreply.github.com>
2023-04-27 20:01:56 +0200

    Commit Inicial
    Estructura básica del coche, con las clases rueda y coche.
    Hacer las clases Motor y Puerta.

---
+++ proyecto_coche/src/Coche.java
@@ -0,0 +1,22 @@
+public class Coche {
+
+    //Propiedades
+    private String marca;
+    private String modelo;
+    private int color;
+    //Asociaciones
+
+    //Constructores
+    public Coche(String marca, String modelo, int color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    //Getters y settters
+
+    //Utilidades
+    //otros métodos
+
+
+}
